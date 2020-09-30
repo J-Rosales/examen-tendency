cd8e99a3191bd7ad0eaec201eff853b680a4bece
Jose Rosales <jarc_lex@hotmail.com>
2020-09-29 21:34:31 -0500

    Updates

---
+++ precache-manifest.d1bc345f17436aa606f1661e414d0a76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc65e4ff1ba21c196337cf7b5af4b3c1",
+    "url": "/examen-tendency/index.html"
+  },
+  {
+    "revision": "447bcb0a8a611b48d716",
+    "url": "/examen-tendency/static/css/2.97c59ffe.chunk.css"
+  },
+  {
+    "revision": "90d17f5c5ac7099d0993",
+    "url": "/examen-tendency/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "447bcb0a8a611b48d716",
+    "url": "/examen-tendency/static/js/2.93d2b9b1.chunk.js"
+  },
+  {
+    "revision": "95bb3c793df2b0b286f335efad710030",
+    "url": "/examen-tendency/static/js/2.93d2b9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d17f5c5ac7099d0993",
+    "url": "/examen-tendency/static/js/main.2b31255b.chunk.js"
+  },
+  {
+    "revision": "9a4e9af126cfa0488186",
+    "url": "/examen-tendency/static/js/runtime-main.419f2761.js"
+  }
+]);
